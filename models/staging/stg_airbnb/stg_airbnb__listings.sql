564e02c331c99a93556b84a0d7ad57ed7cad6292
BBILODIALLO@yahoo.fr <BBILODIALLO@yahoo.fr>
2026-02-11 08:05:21 +0000

    charges airbnb

---
+++ models/staging/stg_airbnb/stg_airbnb__listings.sql
@@ -0,0 +1,18 @@
+with
+source as (
+ select * from {{ source('airbnb', 'listings') }}
+),
+
+renamed as (
+ select
+ cast(id as string) as listing_id,
+ cast(host_id as integer) as host_id,
+cast(neighbourhood_cleansed as string) as neighbourhood_cleansed,
+ cast(room_type as string) as room_type,
+ cast(price as numeric) as price,
+ cast(minimum_nights as integer) as minimum_nights,
+ cast(number_of_reviews as integer) as number_of_reviews,
+ cast(availability_365 as integer) as availability_365
+ from source
+)
+select * from renamed
